7e932030a73ddd6bdb641256e8ff646acbc53be7
vanpeta <carlosallendesanz@gmail.com>
2017-05-25 15:20:47 -0700

    adding route to create posts

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,7 +1,8 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPosts } from '../actions';
-import _ from 'lodash';
 
 class PostsIndex extends Component {
 	componentDidMount() {
@@ -23,6 +24,11 @@ class PostsIndex extends Component {
 	render() {
 		return (
 			<div>
+				<div className="text-xs-right">
+					<Link className="btn btn-primary" to="/posts/new">
+						Add Post
+					</Link>
+				</div>
 				<h3>Post</h3>
 				<ul className="list-group">
 					{this.renderPosts()}
