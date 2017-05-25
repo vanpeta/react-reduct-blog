3f78b705e17e0ce2fe99648b2acd6b78ff4714fc
vanpeta <carlosallendesanz@gmail.com>
2017-05-25 14:57:36 -0700

    rendering posts inside posts_index component

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const FETCH_POSTS = 'fetch_posts';
+export const FETCH_POSTS = 'fetch_posts';
+
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=vanpetakey'
 
