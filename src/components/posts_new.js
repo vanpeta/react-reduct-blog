fe08088d172b07f6428edec9b67e9ea223248a43
vanpeta <carlosallendesanz@gmail.com>
2017-05-26 11:53:01 -0700

    adding style to validators

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -4,14 +4,16 @@ import { Field, reduxForm } from 'redux-form';
 class PostsNew extends Component {
 	
 	renderField(field) {
+		const className = `form-group ${field.meta.touched && field.meta.error ? 'has-danger' : ""}`;
+
 		return (
-			<div className="form-group">
+			<div className={className}>
 				<label>{field.label}</label>
 				<input className="form-control"
 					type="text"
 					{...field.input}
 				/>
-				{field.meta.error}
+				<div className="text-help">{field.meta.touched ? field.meta.error : ''}</div>
 			</div>
 		);
 	}
