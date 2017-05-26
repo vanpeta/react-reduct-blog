8c1e71ec9cfe81b96fab86d08acd8d42af607e37
vanpeta <carlosallendesanz@gmail.com>
2017-05-26 14:33:38 -0700

    wiring up action and reducer to the postshow component

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions/';
 
 class PostsShow extends Component {
+	componentDidMount() {
+		const { id } = this.props.match.params.id;
+		this.props.fetchPost(id);
+	}
+
 	render() {
 		return <div>PostsShow</div>
 	}
 }
 
-export default PostsShow;
+function mapStateToProps({ posts }, ownProps) {
+	return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchPost }) (PostsShow);
+
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,10 +1,17 @@
-import { FETCH_POSTS } from '../actions';
 import _ from 'lodash';
+import { FETCH_POSTS, FETCH_POST } from '../actions';
+
 
 export default function(state = {}, action) {
 	switch (action.type) {
 		case FETCH_POSTS:
 			return _.mapKeys(action.payload.data, 'id');
+		case FETCH_POST:
+			// const post = action.payload.data;
+			// const newState = { ...state };
+			// newState[post.id] = post;
+			// return { ...state, }
+			return { ...state, [action.payload.data.id]: action.payload.data };
 		default:
 			return state
 	}
