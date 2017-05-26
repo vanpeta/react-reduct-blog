9c61abc0ea90f5248770df271156073344c08eac
vanpeta <carlosallendesanz@gmail.com>
2017-05-25 17:26:42 -0700

    formating forms with reduxform on posts_news.js

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+	
+	renderField(field) {
+		return (
+			<div className="form-gropu">
+				<label>{field.label}</label>
+				<input className="form-control"
+					type="text"
+					{...field.input}
+				/>
+			</div>
+		);
+	}
+
 	render() {
 		return (
-			<div>
-				PostsNew!
-			</div>
-		)
+			<form>
+				<Field
+					label="Post Title"
+					name="title"
+					component={this.renderField}
+				/>
+				<Field
+					label="Tags"
+					name="tags"
+					component={this.renderField}
+				/>
+				<Field
+					label="Post Content"
+					name="content"
+					component={this.renderField}
+				/>
+			</form>
+		);
 	}
 }
 
-export default PostsNew;
+
+
+export default reduxForm({ 
+	form: 'PostsNewForm'
+ })(PostsNew);
