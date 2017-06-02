d6df3152265afd9c09945e3758db3f33ac4c5cc7
Carlos Allende <carlosallendesanz@gmail.com>
2017-06-01 21:13:39 -0700

    refactoring

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-export const FETCH_POSTS = 'fetch_posts';
 export const CREATE_POST = 'create_post';
+export const FETCH_POSTS = 'fetch_posts';
 export const FETCH_POST = 'fetch_post';
 export const DELETE_POST = 'delete_post';
 
